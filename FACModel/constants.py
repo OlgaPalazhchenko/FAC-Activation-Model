50312d1a408ee71f3c7837ed19c589f834f22002
OlgaPalazhchenko <opalazhc@unb.ca>
2018-01-26 00:44:51 -0400

    continuing to tune

--- FACModel/constants.py
+++ FACModel/constants.py
@@ -48,7 +48,7 @@ NiDiffusivity = 0.00041
 CoDiffusivity = 0.00041
 
 # Kinetic precipitation/dissolution constants [cm/s]
-KpFe3O4 = 0.22
+KpFe3O4 = 0.1
 KdFe3O4 = 0.1
 # KpFe_Ferrite = 0.014 #same as magnetite precipitation
 # KdFe_Ferrite  = 0.044 'same as magnetite dissolution
